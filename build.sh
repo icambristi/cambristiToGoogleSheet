914bd6dc981f99f3273feab65fa018107fe17dfd
xmayeur <xavier@mayeur.be>
2025-11-17 12:08:21 +0100

    Remove Docker Scout recommendations from `build.sh` and streamline deployment commands

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 docker buildx build --platform linux/amd64,linux/arm64 -t xmayeur/wixgooglesheet . --push && \
-docker scout recommendations xmayeur/wixgooglesheet && \
 ssh -p 5210 contabo 'sudo docker pull xmayeur/wixgooglesheet && \
 sudo docker compose -f /root/docker-compose.yml up -d wixgoogle'
